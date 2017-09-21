3cede949a74e2247046b64b2be8e0c1db703776a
freemanchen <freemanxchen@gmail.com>
2017-09-20 21:35:56 -0700

    works for linkedin automatically

--- insert.js
+++ insert.js
@@ -1,1 +1,15 @@
-// $('body').remove();
+
+
+function openLinkedIn() {
+
+    Element.prototype.remove = function() {
+        this.parentElement.removeChild(this);
+    }
+
+    if (window.location.hostname.includes('linkedin.com')) {
+        document.getElementById('advocate-modal').remove();
+        document.getElementsByTagName('body')[0].classList.remove('advocate-modal-visible');
+    }
+}
+
+openLinkedIn();
---
+++ popup.js
@@ -0,0 +1,9 @@
+// document.getElementById('element-killer').addEventListener('click', function() {
+//     chrome.tabs.getCurrent(function(tab) {
+//         console.log(tab);
+//     });
+// });
+//
+// chrome.tabs.getCurrent(function(tab) {
+//     console.log(tab);
+// });
